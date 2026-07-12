0be027908da9b1b7dd07d1b273e823c5e24dfa8c
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-07-12 18:08:56 +0600

    type of typesctipt

---
+++ index.js
@@ -0,0 +1,7 @@
+const friends =["Alice", "Bob", "Charlie", "David", "Eve"];
+
+// Function to display the list of friends
+const webFriends = [...friends, "Frank", "Grace", "Heidi"];
+console.log("List of friends:", webFriends);
+
+
---
+++ index.ts
@@ -0,0 +1,49 @@
+// let  userName = "salam";
+//  userName =12;
+
+
+//  let age = 20;
+//  age = "twenty";
+
+//  let isStudent = true;
+//  isStudent = 21;
+
+ let data: string[] | null = null;
+ data = ["Hello"];
+
+ let phone: number | undefined = undefined;
+ phone = 1234567890;
+
+
+ const add =(x: number, y: number): number => {
+    return x + y;
+     // This will cause a TypeScript error because the return type is specified as number   
+ }
+
+
+ let data1: unknown = "Hello"; 
+ // unknown type can hold any value, but we need to check its type before using it
+
+ if (typeof data1 === "string") {
+     console.log(data1.toUpperCase());
+ }
+
+
+// ? conditional operator tarnary operator
+ let age: number = 20;
+ let isAdult: boolean = age >= 18 ? true : false;
+ console.log(isAdult); // Output: true
+
+ //?? nullish coalescing operator
+ let userInput: string | null = null;
+ let defaultValue: string = "Default Value";
+ let result: string = userInput ?? defaultValue;
+ console.log(result); // Output: "Default Value"
+
+ //? optional chaining operator
+ interface User {
+    name: string;
+    email?: string; // Optional property
+    }
+
+    
