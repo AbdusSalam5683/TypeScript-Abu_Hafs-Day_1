3f6ea03136fc1addbab69c7738793b512c455536
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-07-14 08:28:53 +0600

    Refactor user data example and clean up unused code

--- day-2/index.ts
+++ day-2/index.ts
@@ -14,16 +14,33 @@
 // console.log(result);
 
 
-const users ={
-    userName: 'sinan',
-    email: 'sinan@mail.io',
-    age: 12,
-    phone:{
-        homePhone: 6552595865,
-        officePhone: 49464932
-    }
+// const users ={
+//     userName: 'dame',
+//     email: 'sinan@mail.io',
+//     age: 12,
+//     phone:{
+//         homePhone: 6552595865,
+//         officePhone: 49464932
+//     }
+// }
+
+// const {userName, email, age, phone:{homePhone, officePhone}} = users;
+
+// console.log(userName, email, age,officePhone)
+
+
+const data:any ="safins";
+
+(data as string).length
+console.log( data.length)
+
+interface IProduct{
+    productName:string,
+    price: number
 }
 
-const {userName, email, age, phone:{homePhone, officePhone}} = users;
 
-console.log(userName, email, age,officePhone)
+const product:IProduct={
+    productName: "HP",
+    price:4554566
+}
