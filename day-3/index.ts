8c9c0b8491700786b5043fa44fc8bf56bc704dc3
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-07-14 08:29:27 +0600

    Add additional user properties and boolean flags for state management

--- day-3/index.ts
+++ day-3/index.ts
@@ -5,3 +5,11 @@ let greeting:string = `My name is ${userName}`
 console.log(userName, massage, greeting);
 
 
+let age: number = 25;
+let price: number = 99.99;
+let negative: number = -10;
+
+let isAdmin: boolean = true;
+let isLoggedIn: boolean = true;
+let isActive: boolean = true;
+
